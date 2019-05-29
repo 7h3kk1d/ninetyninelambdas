36077eb2a2cebd55d87bc7d93f787b4d22120b2a
Alexander Bandukwala <7h3kk1d@gmail.com>
2019-05-29 08:50:06 -0500

    Cleanup Two

--- src/main/java/com/thekkid/ninetyninelambdas/problems/Two.java
+++ src/main/java/com/thekkid/ninetyninelambdas/problems/Two.java
@@ -6,9 +6,12 @@ import static com.jnape.palatable.lambda.functions.builtin.fn1.Head.head;
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Reverse.reverse;
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Tail.tail;
 
+/**
+ * Find the last but one box of a list.
+ */
 public class Two {
 
-    public static <A> Maybe<A> two(Iterable<A> asList) {
-        return head(tail(reverse(asList)));
+    public static <A> Maybe<A> two(Iterable<A> as) {
+        return head(tail(reverse(as)));
     }
 }
