dc9ec5daa961bef6d25eeccaebf9281a5cc897f6
Alexander Bandukwala <7h3kk1d@gmail.com>
2019-10-11 17:57:36 -0500

    Fix twenty two

--- src/main/java/com/thekkid/ninetyninelambdas/problems/TwentyTwo.java
+++ src/main/java/com/thekkid/ninetyninelambdas/problems/TwentyTwo.java
@@ -1,8 +1,8 @@
 package com.thekkid.ninetyninelambdas.problems;
 
 import static com.jnape.palatable.lambda.functions.Fn2.fn2;
-import static com.jnape.palatable.lambda.functions.builtin.fn2.GTE.gte;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.Iterate.iterate;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.LTE.lte;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeWhile.takeWhile;
 
 /**
@@ -10,6 +10,6 @@ import static com.jnape.palatable.lambda.functions.builtin.fn2.TakeWhile.takeWhi
  */
 public class TwentyTwo {
     public static Iterable<Integer> twentyTwo(int lower, int upper) {
-        return takeWhile(gte(upper), iterate(fn2(Integer::sum).apply(1), lower));
+        return takeWhile(lte(upper), iterate(fn2(Integer::sum).apply(1), lower));
     }
 }
