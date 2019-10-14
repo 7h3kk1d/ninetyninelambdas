5b49059ea79769119995c324e93bf2f93c739d90
Alexander Bandukwala <7h3kk1d@gmail.com>
2019-10-14 12:37:49 -0500

    More efficient 23

--- src/main/java/com/thekkid/ninetyninelambdas/problems/TwentyThree.java
+++ src/main/java/com/thekkid/ninetyninelambdas/problems/TwentyThree.java
@@ -1,32 +1,55 @@
 package com.thekkid.ninetyninelambdas.problems;
 
+import com.jnape.palatable.lambda.adt.Maybe;
+import com.jnape.palatable.lambda.adt.hlist.Tuple2;
+import com.jnape.palatable.lambda.functions.Fn1;
 import com.jnape.palatable.lambda.io.IO;
 
 import java.util.Random;
 
+import static com.jnape.palatable.lambda.adt.Maybe.just;
+import static com.jnape.palatable.lambda.adt.Maybe.nothing;
+import static com.jnape.palatable.lambda.adt.hlist.HList.tuple;
 import static com.jnape.palatable.lambda.functions.Fn0.fn0;
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Constantly.constantly;
 import static com.jnape.palatable.lambda.functions.builtin.fn1.Size.size;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Filter.filter;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Into.into;
 import static com.jnape.palatable.lambda.functions.builtin.fn2.LTE.lte;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Map.map;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Unfoldr.unfoldr;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Zip.zip;
 import static com.jnape.palatable.lambda.functions.builtin.fn4.IfThenElse.ifThenElse;
 import static com.jnape.palatable.lambda.io.IO.io;
-import static com.jnape.palatable.lambda.monad.Monad.join;
-import static com.thekkid.ninetyninelambdas.problems.Twenty.twenty;
+import static java.lang.Math.abs;
 
 /**
  * Extract a given number of randomly selected elements from a list
  */
 public class TwentyThree {
     public static <T> IO<Iterable<T>> twentyThree(long n, Iterable<T> ts) {
-        return io(fn0(Random::new))
-                .flatMap(random -> inner(random, n, ts));
+        return whetherToKeep(size(ts), n)
+                .fmap(zip(ts))
+                .fmap(filter(Tuple2::_2))
+                .fmap(map(Tuple2::_1));
     }
 
-    private static <T> IO<Iterable<T>> inner(Random random, long n, Iterable<T> ts) {
-        Long size = size(ts);
-        return ifThenElse(lte(n),
-                constantly(io(ts)),
-                s -> join(io(() -> inner(random, n, twenty(random.nextInt() % s, ts)))))
-                .apply(size);
+    private static IO<Iterable<Boolean>> whetherToKeep(long numOfBooleans, long trueBooleans) {
+        return io(fn0(Random::new))
+                .flatMap(random -> io(() -> unfoldr(into((truesRemaining, sizeRemaining) -> {
+                    if (truesRemaining <= 0L) {
+                        return nothing();
+                    }
+
+                    return io(random::nextLong)
+                            .fmap(j -> abs(j) % sizeRemaining)
+                            .fmap(i -> {
+                                Fn1<Long, Maybe<Tuple2<Boolean, Tuple2<Long, Long>>>> longMaybeFn1 = ifThenElse(lte(truesRemaining),
+                                        constantly(just(tuple(true, tuple(truesRemaining - 1, sizeRemaining - 1)))),
+                                        constantly(just(tuple(false, tuple(truesRemaining, sizeRemaining - 1)))));
+                                return longMaybeFn1.apply(i);
+                            })
+                            .unsafePerformIO();
+                }), tuple(trueBooleans, numOfBooleans))));
     }
 }
--- src/test/java/com/thekkid/ninetyninelambdas/problems/TwentyThreeTest.java
+++ src/test/java/com/thekkid/ninetyninelambdas/problems/TwentyThreeTest.java
@@ -1,8 +1,13 @@
 package com.thekkid.ninetyninelambdas.problems;
 
+import com.jnape.palatable.lambda.adt.Unit;
+import com.jnape.palatable.lambda.io.IO;
 import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.junit.jupiter.api.Test;
 
+import static com.jnape.palatable.lambda.adt.Unit.UNIT;
+import static com.jnape.palatable.lambda.functions.builtin.fn1.Repeat.repeat;
+import static com.jnape.palatable.lambda.functions.builtin.fn2.Take.take;
 import static com.thekkid.ninetyninelambdas.problems.TwentyThree.twentyThree;
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.allOf;
@@ -25,5 +30,13 @@ class TwentyThreeTest {
                         .unsafePerformIO(),
                 allOf(hasItem("a"),
                         iterableWithSize(2)));
+    }
+
+    @Test
+    void stackOverflow() {
+        Iterable<Unit> take = take(50_000_000, repeat(UNIT));
+
+        IO<Iterable<Unit>> iterableIO = twentyThree(10_000_000, take);
+        iterableIO.unsafePerformIO();
     }
 }
